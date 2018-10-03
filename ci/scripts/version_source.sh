6942b11ee1e3d4c0798ec37c7202defbde5b035f
Satish Kumar Andey <SatishKumar_Andey@comcast.com>
2018-10-02 22:43:17 -0600

    commit 2

--- ci/scripts/version_source.sh
+++ ci/scripts/version_source.sh
@@ -2,10 +2,12 @@
 
 set -eux
 
-git clone concourse-web updated-concourse-web
+#git clone concourse-web updated-concourse-web
 
-cd updated-concourse-web
+#cd updated-concourse-web
 
-version=`cat ../version/number`
+#version=`cat ../version/number`
 
-git tag concourse-web-v$version.build
+#git tag concourse-web-v$version.build
+echo "hello world"
+ls -ltra
