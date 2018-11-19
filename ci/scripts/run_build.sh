1c0530c7ac7596b8d5878ee7bd475124b93eced3
Satish Kumar Andey <SatishKumar_Andey@comcast.com>
2018-11-19 15:09:56 -0700

    printing tag commit

--- ci/scripts/run_build.sh
+++ ci/scripts/run_build.sh
@@ -2,22 +2,14 @@
 
 uname -a
 ls -ltra
-
 pwd
-
-ls -ltra .m2
-
 set -e -u -x
 
 git clone concourse-web updated-concourse-web
 cd updated-concourse-web
 set -e -u +x
-echo mvn --version
 
 ls -ltr ../.m2
-
-
-#version=`cat ../version/number`
 
 mvn -Dmaven.repo.local="../.m2" clean install -Dmaven.test.skip=true
 
@@ -29,8 +21,9 @@ git config --global user.name "sandey080"
 git pull origin develop
 echo "Creating tag for the for new checkins in develop branch"
 git tag -a $VERSION -m "Created tag $VERSION for the new checkins in develop"
-
 git push origin $VERSION
+
+git log -3
 
 
 git pull origin develop
@@ -38,11 +31,10 @@ echo "Increment snapshot version for next release"
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT
 
 
-
 VERSION_NEW=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
 
 git add pom.xml
-git commit -m "Updated snapshot with latest version for next release "+$VERSION_NEW
+git commit -m "Updated snapshot with latest version for next release " $VERSION_NEW
 
 git pull origin develop
 echo "Build success and released the SNAPSHOT version "+ $VERSION
