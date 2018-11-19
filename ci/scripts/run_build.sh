1d6f9abbb3aceecb577c039d7185105592afed89
Satish Kumar Andey <SatishKumar_Andey@comcast.com>
2018-11-19 14:45:49 -0700

    tag changes

--- ci/scripts/run_build.sh
+++ ci/scripts/run_build.sh
@@ -23,11 +23,22 @@ mvn -Dmaven.repo.local="../.m2" clean install -Dmaven.test.skip=true
 
 VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
 
+echo "Creating tag for the for new checkins in develop branch"
+git tag -a $VERSION-SNAPSHOT -m "Created tag "+$VERSION +" for the new checkins in develop"
+
+
+
 echo "Increment snapshot version for next release"
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT
 
-#git add pom.xml
-#git commit -m "Updated snapshot with latest version"+$VERSION
+git config --global user.email "satishkumar_andey@comcast.com"
+git config --global user.name "sandey080"
+
+VERSION_NEW=$(mvn org.apache.maven.plugins:maven-help-plugin:3.1.0:evaluate -Dexpression=project.version -q -DforceStdout)
+
+git add pom.xml
+git commit -m "Updated snapshot with latest version for next release "+$VERSION_NEW
+
 
 echo "Build success and released the SNAPSHOT version "+ $VERSION
 
